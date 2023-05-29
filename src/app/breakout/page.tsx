2532047b913b8a7358e520686e144c8a3a36d5d5
Aaron McAdam <aaron@aaronmcadam.com>
2023-05-29 22:14:29 +0100

    breakout working

--- src/app/breakout/page.tsx
+++ src/app/breakout/page.tsx
@@ -48,14 +48,15 @@ export default function Breakout() {
   const [blocks, setBlocks] = useState<Block[]>(initialBlocks);
   const [paddlePosition, setPaddlePosition] = useState(paddleStart);
   const [ballPosition, setBallPosition] = useState(ballStart);
+  const [score, setScore] = useState(0);
   const [result, setResult] = useState("");
   const ballAnimationRef = useRef<number | null>(null);
-  const xDirectionRef = useRef(2);
+  const xDirectionRef = useRef(-2);
   const yDirectionRef = useRef(2);
 
   useEffect(() => {
     function movePlayer(event: KeyboardEvent) {
-      if (result === "GAME_OVER") {
+      if (result === "GAME_OVER" || result === "GAME_WON") {
         return;
       }
 
@@ -114,6 +115,7 @@ export default function Breakout() {
   }, []);
 
   // change direction
+  // ensure that the direction change occurs only once per collision.
   useEffect(() => {
     const [xAxis, yAxis] = ballPosition;
 
@@ -132,23 +134,52 @@ export default function Breakout() {
     const [xAxis, yAxis] = ballPosition;
 
     for (let i = 0; i < blocks.length; ++i) {
-      if (
-        xAxis > blocks[i].bottomLeft[0] &&
-        xAxis < blocks[i].bottomRight[0] &&
-        yAxis > blocks[i].bottomLeft[1] &&
-        yAxis < blocks[i].topLeft[1]
-      ) {
+      const block = blocks[i];
+      const [left, right, top, bottom] = [
+        block.bottomLeft[0],
+        block.bottomRight[0],
+        block.topLeft[1],
+        block.bottomLeft[1],
+      ];
+
+      if (xAxis >= left && xAxis <= right && yAxis >= bottom && yAxis <= top) {
         console.log("we hit a block");
         setBlocks((prev) => prev.filter((_block, index) => index !== i));
-
-        // Reverse the ball direction upon block collision
-        xDirectionRef.current = -xDirectionRef.current;
-        yDirectionRef.current = -yDirectionRef.current;
+        changeDirection();
+        setScore((prev) => prev + 1);
 
         break; // exit the loop after the first hit
       }
     }
   }, [ballPosition, blocks]);
+
+  // check for paddle collision
+  useEffect(() => {
+    const [ballXAxis, ballYAxis] = ballPosition;
+    const [paddleXAxis, paddleYAxis] = paddlePosition;
+
+    // try to stop the ball getting stuck in the paddle
+    if (
+      ballXAxis + xDirectionRef.current > paddleXAxis &&
+      ballXAxis + xDirectionRef.current < paddleXAxis + BLOCK_WIDTH &&
+      ballYAxis + yDirectionRef.current > paddleYAxis &&
+      ballYAxis + yDirectionRef.current < paddleYAxis + BLOCK_HEIGHT
+    ) {
+      yDirectionRef.current = -yDirectionRef.current;
+    }
+  }, [ballPosition, paddlePosition]);
+
+  function changeDirection() {
+    if (xDirectionRef.current === 2 && yDirectionRef.current === 2) {
+      yDirectionRef.current = -2;
+    } else if (xDirectionRef.current === 2 && yDirectionRef.current === -2) {
+      xDirectionRef.current = -2;
+    } else if (xDirectionRef.current === -2 && yDirectionRef.current === -2) {
+      yDirectionRef.current = 2;
+    } else if (xDirectionRef.current === -2 && yDirectionRef.current === 2) {
+      xDirectionRef.current = 2;
+    }
+  }
 
   // handle game over
   useEffect(() => {
@@ -164,11 +195,24 @@ export default function Breakout() {
     }
   }, [ballPosition]);
 
+  // handle win
+  useEffect(() => {
+    if (blocks.length === 0) {
+      if (ballAnimationRef.current) {
+        window.clearInterval(ballAnimationRef.current);
+      }
+
+      // reset game state
+      setResult("GAME_WON");
+    }
+  }, [blocks.length]);
+
   return (
     <div className="py-8 h-full flex flex-col">
       <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
         Breakout
       </h2>
+      <div>Score: {score}</div>
       <div>Result: {result}</div>
       <div className="relative h-[300px] w-[560px]">
         {blocks.map((block, index) => (
